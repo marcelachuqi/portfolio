d49329adf79dc21e982f72659710632ac32788c4
Marcela Chuqi <81633985+marcelachuqi@users.noreply.github.com>
2021-08-11 15:46:14 +0200

    add team work 3

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,6 +5,10 @@ import netflix from "../image/netflix.png";
 import anonimus from "../image/anonimus.jpeg";
 import random from "../image/random.png";
 import heroku from "../image/heroku.png";
+import etiqueta1 from "../image/etiqueta1.png";
+import nombrelogo from "../image/nombre-logo.png";
+import etiqueta2 from "../image/etiqueta2.png";
+import logosimple from "../image/logosimple2.png";
 
 const Projects = () => {
   return (
@@ -47,19 +51,59 @@ const Projects = () => {
           <h3 className="project-title">Random Number</h3>
           <p>
             A responsive website layout. HTML -CSS -Sass -Node -Animation -Gulp/
-            Partials / BEM{" "}
+            Partials / BEM
           </p>
         </div>
-        <h2 className="field2">Work Team</h2>
+        <h2 className="field2">Team work</h2>
         <div className="workteam">
+          <a href=" http://beta.adalab.es/project-promo-m-module-1-team-3/">
+            <section className="image">
+              <div className="back">
+                <div className="animation">
+                  <div>
+                    <img
+                      className="etiqueta1"
+                      src={etiqueta1}
+                      alt="etiqueta1"
+                      title="etiqueta1"
+                    />
+                  </div>
+                  <div>
+                    <img
+                      className="nombre-logo"
+                      src={nombrelogo}
+                      alt="Logo eleven code"
+                      title="Logo Completo"
+                    />
+                  </div>
+                  <div>
+                    <img
+                      className="etiqueta2"
+                      src={etiqueta2}
+                      alt="etiqueta2"
+                      title="etiqueta2"
+                    />
+                  </div>
+                  <div>
+                    <img
+                      className="logosimple"
+                      src={logosimple}
+                      alt="Logo simple"
+                      title="Logo simple"
+                    />
+                  </div>
+                </div>
+              </div>
+            </section>
+          </a>
+          <h3 className="project-title">Eleven Code</h3>
+          <p> Gulp | Animation Css| Partials </p>
+
           <a href="http://happi-painters.herokuapp.com/#/ ">
             <img className="heroku" src={heroku} />
           </a>
           <h3 className="project-title">hAPpI Painters</h3>
-          <p>Backend, SQL</p>
-          <a href=" http://beta.adalab.es/project-promo-m-module-1-team-3/"></a>
-          <h3 className="project-title">Eleven Code</h3>
-          <p>Mockup</p>
+          <p>Backend | SQL</p>
         </div>
       </div>
     </>
